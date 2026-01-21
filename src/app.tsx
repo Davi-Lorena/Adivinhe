c65ea1aa05130747432b44a0b82d72e68d6aec98
Davi Lorena <davilorena2016@gmail.com>
2026-01-21 11:20:37 -0300

    feat: adiciona componente de letra

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,6 @@
 import { Header } from "./components/Header"
 import { Tip } from "./components/Tip"
+import { Letter } from "./components/Letter"
 
 import styles from "./app.module.css"
 
@@ -15,6 +16,7 @@ return (
     <main>
     <Header current={5} max={10} onRestart={handleRestartGame} />
     <Tip tip="Uma das linguagem de programação dinâmica" />
+    <Letter value="a"/>
     </main>
   </div>
 )
