6810a984dc9b763fe2126f1aad0eb1b2e3724be1
Davi Lorena <davilorena2016@gmail.com>
2026-01-21 15:12:26 -0300

    fix: corrige propriedade rest do botao

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -5,7 +5,7 @@ type Props = React.ComponentProps<"button"> & {
 }
 
 export function Button ({title, ...rest}: Props) {
-return <button type="button" className={styles.button}>
+return <button type="button" className={styles.button} {...rest}>
     {title}
 </button>
 }
