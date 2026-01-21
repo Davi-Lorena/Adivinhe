f5c76823edb8215401fe0967b1c91744770fb0bc
Davi Lorena <davilorena2016@gmail.com>
2026-01-21 13:53:34 -0300

    feat: adiciona variacoes no estilo das letras palpitadas

--- src/components/LettersUsed/index.tsx
+++ src/components/LettersUsed/index.tsx
@@ -5,6 +5,12 @@ export function LettersUsed() {
     return (
         <div className={styles.lettersUsed}>
             <h5>Letras utilizadas</h5>
+
+            <div>
+                <Letter value="Y" size="small" color="wrong"/>
+                 <Letter value="Y" size="small" color="correct"/>
+                  <Letter value="Z" size="small"/>
+            </div>
         </div>
     )
 }
