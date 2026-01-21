d8015dd3b34d787a69ecc0528df201d409fe3ef4
Davi Lorena <davilorena2016@gmail.com>
2026-01-21 11:59:16 -0300

    feat: adiciona componente button

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,7 @@ import { Header } from "./components/Header"
 import { Tip } from "./components/Tip"
 import { Letter } from "./components/Letter"
 import { Input } from "./components/Input"
+import { Button } from "./components/Button"
 
 import styles from "./app.module.css"
 
@@ -26,8 +27,9 @@ return (
     </div>
 
     <h4>Palpite</h4>
-    <div>
+    <div className={styles.guess}>
 <Input autoFocus maxLength={1} placeholder="?"/>
+<Button title="Confirmar"/>
     </div>
     </main>
   </div>
