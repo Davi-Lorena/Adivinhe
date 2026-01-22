6e27f194faf26a65c5ee18ec56be2df2eae5c917
Davi Lorena <davilorena2016@gmail.com>
2026-01-22 18:54:15 -0300

    feat: adiciona metodo para reiniciar o jogo

--- src/app.tsx
+++ src/app.tsx
@@ -19,7 +19,9 @@ const [challenge, setChallenge] = useState<Challenge | null>(null)
 const ATTEMPTS_MARGIN = 5
 
 function handleRestartGame() {
-  alert("Reiniciar o jogo")
+  const isConfirmed = window.confirm("Tem certeza que deseja reiniciar o jogo?")
+
+  if(isConfirmed) startGame()
 }
 
 function startGame() {
